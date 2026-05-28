4d2d644f6b21f799a6fcc4ba601dcd4cf3d25fb6
Benjamin Chiang <chiangbenjamin991@gmail.com>
2026-05-28 15:47:58 -0700

    feat(typescript): throw error

--- packages/typescript/index.mjs
+++ packages/typescript/index.mjs
@@ -1,6 +1,8 @@
 import { defineConfig } from 'eslint/config';
 import tseslint from 'typescript-eslint';
 
+throw Error('ahh');
+
 export default defineConfig(
   {
     'name': 'forvais/plugin/typescript-eslint/overrides',
