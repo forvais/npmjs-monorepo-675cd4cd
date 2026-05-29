11f3a8ab102fe46095050ec020fa574dbd2bea43
Benjamin Chiang <chiangbenjamin991@gmail.com>
2026-05-28 22:14:00 -0700

    feat: enable return await

--- packages/typescript/index.mjs
+++ packages/typescript/index.mjs
@@ -166,7 +166,7 @@ export default defineConfig(
       'typescript-eslint/require-await': 'error',
       'typescript-eslint/restrict-plus-operands': 'error',
       'typescript-eslint/restrict-template-expressions': 'error',
-      'typescript-eslint/return-await': 'off',
+      'typescript-eslint/return-await': 'error',
       'typescript-eslint/strict-boolean-expressions': 'error',
       'typescript-eslint/strict-void-return': 'error',
       'typescript-eslint/switch-exhaustiveness-check': 'error',
