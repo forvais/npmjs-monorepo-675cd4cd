c97936e00b7ce8f341f7925f9624b565a0da13c5
Benjamin Chiang <chiangbenjamin991@gmail.com>
2026-05-31 11:17:27 -0700

    feat: mass changes

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,6 @@
 import { defineConfig } from 'eslint/config';
 
-import js from './packages/javascript/index.mjs';
+import js from './packages/base/index.mjs';
 import ts from './packages/typescript/index.mjs';
 
 export default defineConfig(js, ts);
